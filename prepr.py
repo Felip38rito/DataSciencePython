131c0dfec3754247b8a826ddb78d2aad4fb5633e
Felip38rito <felipe.correia.wd@gmail.com>
2019-03-17 22:23:27 -0300

    ajuste no preprocessamento

--- prepr.py
+++ prepr.py
@@ -1,27 +1,36 @@
-#%% [markdown]
-# # Curso Machine Learning com python
-# ### Limpeza dos dados da base (teste) de crédito
+# Pre processamento necessario para a base de dados de credito (csv)
 
-# Primeiro importamos os dados
-
-#%% 
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, Imputer
+from sklearn.preprocessing import Imputer, StandardScaler
 
-bcred = pd.read_csv("credit-data.csv")
-bcred.describe()
+base = pd.read_csv("credit-data.csv")
 
-#%% [markdown]
-# Percebemos que há valores inválidos em age. 
-# Vamos preencher valores faltantes com a media e corrigir valores negativos
+# Podemos ver que na base há problemas com idade negativa. Vamos substituir os invalidos e os faltantes com a media (dos validos)
+media_idade = base.loc[ base.age > 0 ].age.mean()
 
-#%% 
-# Calculo a media de idade dos validos
-media = bcred['age'][bcred.age > 0].mean()
-# substituo os incorretos
-bcred.loc[ bcred.age < 0, 'age' ] = media
+# substitui os incorretos (<= 0)
+base.loc[ base.age <= 0, 'age' ] = media_idade
 
-# bcred.describe()
+# Vamos agora separar nossa análise em 2 grupos: previsores e atributos classe. 
+# Vamos trabalhar diretamente com as matrizes numericas do numpy
+prev = base.iloc[:, 1:4].values
+post = base.iloc[:, 4].values
 
+# Valores faltantes: Podemos preencher em todas as colunas ao mesmo tempo com Imputer
+imputer = Imputer(axis=0)
+imputer = imputer.fit(prev[:, 0:3])
+prev[:, 0:3] = imputer.transform(prev[:, 0:3])
 
-#%%
+# prev[0:5]
+
+# Vamos padronizar a escala dos atributos, para evitar comportamentos ruins nos estimadores
+# Isso torna nossas analises mais robustas contra outliers
+# Este metodo usa padronizacao (standardization)
+scaler = StandardScaler()
+prev = scaler.fit_transform(prev)
+
+# Agora nossos atributos previsores estao em um formato adequado para os algoritmos:
+# Uma matriz do numpy, com escala padronizada.
+# No caso dessa base de dados nao temos variaveis categoricas, apenas numericas de fato. 
+# Nao precisaremos da etapa do labelencoder
+
