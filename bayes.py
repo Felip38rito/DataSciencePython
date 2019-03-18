c37f1bc47481a329a3c210e464649ef75e0479d3
Felip38rito <felipe.correia.wd@gmail.com>
2019-03-17 22:53:41 -0300

    Adição do arquivo de Naive Bayes: o primeiro método que vamos estudar

---
+++ bayes.py
@@ -0,0 +1,11 @@
+#%% [markdown] 
+# # Naive Bayes com sklearn  
+# Exemplos típicos do uso de Naive Bayes:
+# - Filtros de spam
+# - Mineração de sentimentos
+# - Separação de Documentos
+#
+# Este algoritmo é um classificador probabilístico, baseado no *Teorema de Bayes*
+# $$ P(A|B) = \frac{P(B|A)P(A)}{P(B)} $$
+
+
