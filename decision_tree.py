425e6375fc14cffda03b783b09d594f82b7e4b20
Felipe Correia <felipe.correia.wd@gmail.com>
2019-05-01 00:28:13 -0300

    Arvore de decisao - classificador por entropia

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,15 @@
+#%%[markdown]
+# # Árvores de Decisão
+# O classificador por árvores de decisão funciona com base em teoria da informação.
+# Primeiramente, estima-se o ganho de informacao por cada variavel preditora
+
 #%% 
 # Algoritmos de arvores de decisao para prever dados
 # classificador por arvores de decisao
 from sklearn.tree import DecisionTreeClassifier, export
 from db_loaders.risco_credito import previsores, risco
 
+# Classificador por arvores de decisao
 modelo = DecisionTreeClassifier(criterion='entropy')
 modelo.fit(previsores, risco)
 
